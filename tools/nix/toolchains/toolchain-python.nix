9c83283d6468b66daec7215d42b7f8c9250ca6cd
supermaxiste <supermegaiperste@hotmail.com>
2026-07-20 11:34:32 +0200

    fix(ci): removed line converting to string

--- tools/nix/toolchains/toolchain-python.nix
+++ tools/nix/toolchains/toolchain-python.nix
@@ -20,6 +20,32 @@
         pkgs.libz # fix: for numpy/pandas import
       ];
 
+      # We use `devenv` language support since, its
+      # pretty involved to setup a python environment.
+      # FIXME: Any `python313.withPackages` expression
+      # cannot be used for creating a virtual environment (venv, uv etc.)
+      # Until this is fixed: https://github.com/NixOS/nixpkgs/pull/442540).
+      # See also https://github.com/astral-sh/uv/issues/16106
+      languages.python = {
+        enable = true;
+
+        # Heavy modules relying (CYTHON, ext. shared libraries etc)
+        # should be built by Nix.
+        package = pkgs.python313.withPackages (p: [
+          p.numpy
+        ]);
+
+        venv.enable = true;
+        uv = {
+          enable = true;
+          package = pkgs.uv;
+          sync = {
+            enable = true;
+            allExtras = true;
+          };
+        };
+      };
+
       env = {
         RUFF_CACHE_DIR = ".output/cache/ruff";
       };
